a53bdb686d5d6324774e5a77c71f1c9e9327268b
Andrew Repine <repineap@rose-hulman.edu>
2023-10-20 13:16:42 -0400

    Moved map functionality to make it more consistien

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,17 @@
 let map;
 
+(g=>
+    {var h,a,k,p="The Google Maps JavaScript API",
+    c="google",l="importLibrary",q="__ib__",
+    m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),
+    r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(
+        async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");
+        for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);
+        a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));
+        a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));
+        d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
+        ({key: "AIzaSyAI6ezp2EJ9_ax8nEEGWU39I3EON_hEaLA", v: "beta"});
+
 async function initMap(){
     const position = {lat: 39.4667, lng: -87.4139}
     const { Map } = await google.maps.importLibrary("maps");
