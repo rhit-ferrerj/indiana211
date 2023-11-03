3dd1f3e9ee328946563507d9c25f86d5c8f89580
ferrerjulian <ferrerj@rose-hulman.edu>
2023-11-03 11:01:39 -0400

    Api

--- public/server/server.js
+++ public/server/server.js
@@ -7,38 +7,62 @@ app = express();
 
 const jsonData = JSON.parse(fs.readFileSync('realResources.json', 'utf-8'));
 
-app.get('/data/county/:county', (req, res) => {
-    const cty = req.params.county;
-    const filteredData = jsonData.filter((item) => item.county === cty);
+// app.get('/data/county/:county', (req, res) => {
+//     const cty = req.params.county;
+//     const filteredData = jsonData.filter((item) => item.county === cty);
 
-    if (filteredData.length > 0){
-        res.json(filteredData);
-    } else {
-        res.status(404).json({error: 'No data found for requested county'});
+//     if (filteredData.length > 0){
+//         res.json(filteredData);
+//     } else {
+//         res.status(404).json({error: 'No data found for requested county'});
+//     }
+// });
+
+// app.get('/data/zip/:zip', (req, res) => {
+//     const z = req.params.zip;
+//     const filteredData = jsonData.filter((item) => item.zipcode === z);
+
+//     if (filteredData.length > 0){
+//         res.json(filteredData);
+//     } else {
+//         res.status(404).json({error: 'No data found for requested county'});
+//     }
+// });
+
+// app.get('/data/service', (req, res) => {
+//     const {svc} = req.query;
+
+//     if (svc) {
+//         const svcList = Array.isArray(svc) ? svc : [svc];
+//         const filteredData = jsonData.filter(item => svcList.includes(item.service_name));
+//         res.json(filteredData);
+//     } else {
+//         res.json(jsonData);
+//     }
+// });
+
+
+app.get('/data', (req, res) => {
+
+    const {county, zip, svc} = req.query;
+
+    let filteredData = jsonData;
+
+    if(county) {
+        console.log(county);
+        filteredData = filteredData.filter(item => item.county === county);
     }
-});
 
-app.get('/data/zip/:zip', (req, res) => {
-    const z = req.params.zip;
-    const filteredData = jsonData.filter((item) => item.zipcode === z);
-
-    if (filteredData.length > 0){
-        res.json(filteredData);
-    } else {
-        res.status(404).json({error: 'No data found for requested county'});
+    if(zip) {
+        console.log(zip);
+        filteredData = filteredData.filter(item => item.zipcode === zip);
     }
-});
-
-app.get('/data/service', (req, res) => {
-    const {svc} = req.query;
 
     if (svc) {
         const svcList = Array.isArray(svc) ? svc : [svc];
-        const filteredData = jsonData.filter(item => svcList.includes(item.service_name));
-        res.json(filteredData);
-    } else {
-        res.json(jsonData);
+        filteredData = filteredData.filter(item => svcList.includes(item.service_name));
     }
+    res.json(filteredData);
 });
 
 app.listen(port, () => {
